663a5f9613f7688817f5aecdb6fb60efc261ac83
Lav5588 <lavkumaryadavanshu@gmail.com>
2024-11-08 15:11:34 +0530

    final commit on test

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,7 @@
 
-console.log(localStorage.getItem('username'));
+// console.log(localStorage.getItem('username'));
+
+const githubUserNames = [];
 
 const welcomeElement = document.getElementById('welcome');
 welcomeElement.innerHTML = `welcome ${localStorage.getItem('username')}`;
@@ -8,10 +10,18 @@ const fetchBtn = document.getElementById('fetchBtn');
 
 const fetchUsers = async() =>{
     try{
-        const response = await fetch('https://api.github.com/users?per_page=10');
-        clg(response)
+        const response = await fetch("https:/api.github.com/users?per_page=10");
+        // console.log(response)
         const data = await response.json();
-        console.log(data);
+        
+        const ul = document.getElementById('userLists');
+        data.forEach(ele => {
+            githubUserNames.push(ele.login);
+            const li = document.createElement('li');
+            li.innerHTML = ele.login;
+            ul.appendChild(li);
+        });
+        // console.log(data);
     }
     catch(err){
         console.log(err);
@@ -19,3 +29,34 @@ const fetchUsers = async() =>{
 }
 
 fetchBtn.addEventListener('click',fetchUsers)
+
+
+const logout = async() =>{
+    localStorage.removeItem('username');
+    window.location.replace('login.html')
+}
+
+const logoutBtn = document.getElementById('logout');
+
+logoutBtn.addEventListener('click',logout)
+
+
+const selectEle = document.getElementById('sorting');
+
+const sorting = () =>{
+    if(selectEle.value == 'yes' && githubUserNames.length > 1){
+        githubUserNames.sort();
+        const ul = document.getElementById('userLists');
+        ul.innerHTML = '';
+        githubUserNames.forEach(ele => {
+            const li = document.createElement('li');
+            li.innerHTML = ele;
+            ul.appendChild(li);
+        });
+
+    }
+    // console.log(githubUserNames)
+}
+
+
+selectEle.addEventListener('change',sorting)
